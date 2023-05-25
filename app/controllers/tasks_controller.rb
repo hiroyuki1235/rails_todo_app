4d1ad3a963ffa59fc5a6dd804366e6f78d33b343
“Hiroyuki <hiroyuki.akiho@appirits.com>
2023-05-25 11:52:45 +0900

    edit-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update]
+
   def index
     @tasks = Task.where(status: 0)
   end
 
   def new
     @task = Task.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,10 +23,26 @@ class TasksController < ApplicationController
     end
   end
 
+  def update
+    @task.update(task_params)
+
+    if @task.save
+      redirect_to tasks_path, success: 'タスクを更新しました。'
+    else
+      flash.now[:danger] = 'タスク名を入力してください。'
+      render 'edit'
+    end
+  end
+
 
   private
 
     def task_params
       params.require(:task).permit(:name)
     end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
 end
