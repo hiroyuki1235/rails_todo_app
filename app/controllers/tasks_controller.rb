03b72828d3b18c959424f53fef4d4af3a57dc6e5
“Hiroyuki <hiroyuki.akiho@appirits.com>
2023-05-25 11:06:37 +0900

    create-new-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.where(status: 0)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクを新規作成しました。'
+      redirect_to tasks_path
+    else
+      flash.now[:error] = @task.errors.full_messages
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def task_params
+      params.require(:task).permit(:name)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_043128) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_014629) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
